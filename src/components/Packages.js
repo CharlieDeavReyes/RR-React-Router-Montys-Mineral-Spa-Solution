d27b64ac2b4ed757416d7d55900f61151da91f2f
charlie reyes <charliereyes1228@gmail.com>
2022-10-27 19:48:16 -0700

    added bootstrap styling

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,33 @@
-
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
 
 export default function Packages(props) {
     const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
+        // <div>
+        //     <div className="packages">
+        //         <div className="packagesHeader">
+        //             <h1>Our Packages</h1>
+        //         </div>
+        //         <ul>
+        //             {displayPackages}
+        //         </ul>
+        //     </div>
+        // </div>
+        <Container>
+            <Card>
+                <Card.Title>Our Packages </Card.Title>
+                <Card.Text>Check out some of our packages! Every package is unethically sourced and inorganic</Card.Text>
+                <ListGroup>
                     {displayPackages}
-                </ul>
-            </div>
-        </div>
+                </ListGroup>
+            </Card>
+        </Container>
+
+
+
+
     )
 }
